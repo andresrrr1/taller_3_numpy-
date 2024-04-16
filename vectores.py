4ae656f090edb918756ddd49fb2405cade632ca7
andresrrr1 <andresromero5455@gmail.com>
2024-04-15 21:56:04 -0500

    v 1.0.0

--- vectores.py
+++ vectores.py
@@ -1,1 +1,12 @@
 import numpy as np
+
+a=np.array([[5, 9], [7, 8]])
+
+valores= np.linalg.eig(a)[0]  
+vectores = np.linalg.eig(a)[1] 
+
+print("Valores propios")
+print(valores) #representan la escala de la transformación que la matriz realiza en ciertas direcciones
+print("Vectores propios")
+print(vectores) # solo escala el vector sin cambiar su dirección
+
